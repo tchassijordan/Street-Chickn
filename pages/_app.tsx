a379102ea69d45113be9c2c4a063c2d76b2ba9cd
tchassijordan <tchassijordan@gmail.com>
2022-12-07 14:51:21 +0100

    feat: create app layout with navbar and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import MainLayout from '../layout/Main';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Street Chick&apos;n</title>
+        <meta
+          name='description'
+          content="Street Chick'n website"
+        />
+        <link
+          rel='icon'
+          href='/favicon.ico'
+        />
+        <meta
+          name='viewport'
+          content='initial-scale=1, width=device-width'
+        />
+      </Head>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </div>
+  );
 }
