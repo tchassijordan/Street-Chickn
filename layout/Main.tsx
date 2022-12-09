9cfe31ad23391bbda8998ef2def44cc474551b68
tchassijordan <tchassijordan@gmail.com>
2022-12-09 11:08:10 +0100

    feat: add nav links to MainLayout and send it as props

--- layout/Main.tsx
+++ layout/Main.tsx
@@ -1,10 +1,18 @@
-import Navbar from './Navbar';
+import { Navbar } from './Navbar';
 import Footer from './Footer';
 
-export default function MainLayout({ children }: any) {
+type TLayoutProps = {
+  children: React.ReactElement;
+};
+
+export default function MainLayout({ children }: TLayoutProps) {
+  const navbarLinks = [
+    { name: 'Home', href: '/' },
+    { name: 'Menu', href: '/menu' }
+  ];
   return (
     <div>
-      <Navbar />
+      <Navbar navLinks={navbarLinks} />
       {children}
       <Footer />
     </div>
