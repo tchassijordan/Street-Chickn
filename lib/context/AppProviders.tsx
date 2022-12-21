75b382dc263b8e6f468323f0169ab81d2dc6aeb1
tchassijordan <tchassijordan@gmail.com>
2022-12-21 20:50:04 +0100

    feat: add cart provider to app providers

--- lib/context/AppProviders.tsx
+++ lib/context/AppProviders.tsx
@@ -1,20 +1,14 @@
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CartProvider } from './CartProvider';
+import ThemeProvider from './ThemeProvider';
 
 type TAppProvidersProps = {
   children: React.ReactNode;
 };
 
 export default function AppProviders({ children }: TAppProvidersProps) {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#c1272d'
-      },
-      secondary: {
-        main: '#fbb03b'
-      }
-    }
-  });
-
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <CartProvider>{children}</CartProvider>
+    </ThemeProvider>
+  );
 }
